2b2a3c7486c9bba1b32b0dab1cb6a8139ed9754c
Richard Hanten <Richard.Hanten@uni-tuebingen.de>
2016-08-05 00:34:44 +0200

    started of with static clustering

---
+++ include/kdtree/array_clustering.hpp
@@ -0,0 +1,9 @@
+#ifndef ARRAY_CLUSTERING_HPP
+#define ARRAY_CLUSTERING_HPP
+namespace kdtree {
+template<typename Type, int Dimension>
+struct ArrayClustering {
+
+};
+}
+#endif // ARRAY_CLUSTERING_HPP
---
+++ include/kdtree/page_clustering.hpp
@@ -0,0 +1,12 @@
+#ifndef PAGE_CLUSTERING_HPP
+#define PAGE_CLUSTERING_HPP
+namespace kdtree {
+
+#include "page.hpp"
+
+template<typename Type, int Dimension>
+struct PageClustering {
+
+};
+}
+#endif // PAGE_CLUSTERING_HPP
